ca775ab04536ea6966a8ff171ed3813dea0ddf3b
Arpana998 <arpanachavan998@gmail.com>
2023-01-23 11:59:22 -0800

    props

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,10 +1,15 @@
-const ExpenseItem = () => {
+import './ExpenseItem.css';
+
+const ExpenseItem = (props) => {
+    
     return (
-        <div>
-            <h2>Expense Item</h2>
-            <p>Food Rs 10</p>
-            <p>Petrol Rs 100</p>
-            <p>Movies Rs 200</p>
+        <div className='expense-item'>
+            <div>{props.date.toISOString()}</div>
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div>{props.location}</div>
+                <div className='expense-item__price'>${props.amount}</div>
+            </div>
         </div>
     )
 }
