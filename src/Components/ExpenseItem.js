4ec7192795cba63eb80d06da0ff98eb55b6222f4
Arpana998 <arpanachavan998@gmail.com>
2023-01-24 01:00:59 -0800

    ExpenseDetails

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,17 +1,14 @@
-import './ExpenseItem.css';
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+import ExpenseDetails from "./ExpenseDetails";
 
 const ExpenseItem = (props) => {
-    
-    return (
-        <div className='expense-item'>
-            <div>{props.date.toISOString()}</div>
-            <div className='expense-item__description'>
-                <h2>{props.title}</h2>
-                <div>{props.location}</div>
-                <div className='expense-item__price'>${props.amount}</div>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <ExpenseDetails title={props.title} location={props.location} amount={props.amount} />
+    </div>
+  );
+};
 
 export default ExpenseItem;
